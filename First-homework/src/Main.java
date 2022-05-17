1dca7ab1ee97853e52463c4fe38a38bdbff36079
UniFail <vika.lolkova@mail.ru>
2022-05-17 22:50:08 +0700

    First tasks

---
+++ First-homework/src/Main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    //First task
+    byte   TestByte  = 10;
+    short  TestShort = 100;
+    int    TestInt   = 1000;
+    long   TestLong  = 10000000;
+    float  TestFloat = 100.40f;
+    double TestDouble = 100.20000;
+
+    //Second task
+    float WeightFirstAtl  = 78.2f;
+    float WeightSecondAtl = 82.7f;
+
+    float  SumWeight = WeightFirstAtl + WeightSecondAtl;
+    System.out.
+}
