1eced6ff1d88b1ef16646cbf4a511ac061e3b0e3
UniFail <vika.lolkova@mail.ru>
2022-05-24 00:06:37 +0700

    Second task

--- HW_1_4/src/Main.java
+++ HW_1_4/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //task first
+        //first task
         int i = 1;
         while (i <= 10)
             System.out.print(i++ + " ");
@@ -8,5 +8,13 @@ public class Main {
         for (int j = 10; j != 0; j--)
             System.out.print(j + " ");
             System.out.println("\n");
+
+        //second task
+
+        int firstFriday = 5;
+        for (; firstFriday <= 31 ; firstFriday = firstFriday + 7) {
+            System.out.println("Friday " + firstFriday + "th");
+            }
+
     }
 }
