96ff93d5e3964067d6a8ecd5f52caf124a224da4
UniFail <vika.lolkova@mail.ru>
2022-05-23 23:38:05 +0700

    First task

---
+++ HW_1_4/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //task first
+        int i = 1;
+        while (i <= 10)
+            System.out.print(i++ + " ");
+            System.out.println("\n");
+        for (int j = 10; j != 0; j--)
+            System.out.print(j + " ");
+            System.out.println("\n");
+    }
+}
